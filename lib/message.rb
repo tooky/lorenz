95451251ded7b526631323ef5a67a6c87103e395
Steve Tooke <steve.tooke@gmail.com>
2010-09-29 21:42:07 +0100

    rename remap variable to show its a message

--- lib/message.rb
+++ lib/message.rb
@@ -3,8 +3,8 @@ class Message
     @characters = message.scan(/./)
   end
 
-  def remap_against(text, &block)
-    characters.each_index.inject(text) do |message, index|
+  def remap_against(message, &block)
+    characters.each_index.inject(message) do |message, index|
       block.call(message, index)
     end
   end
